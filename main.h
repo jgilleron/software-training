689a9dfe02e0cd674a83ec2bb3a60cf468614c9c
Jerome Gilleron <jgilleron@gatech.edu>
2017-09-30 16:01:48 -0400

    added a file/

---
+++ main.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int add_square(int a, int b){
+    int r;
+    r = a*a + b*b;
+    return r;
+}
+
---
+++ testFile.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jgill on 9/30/2017.
+//
+
+#include main.h
+using namespace std;
+
+int main(){
+    int z;
+    z = add_square(2,3);
+    cout << "the result is" << z;
+}
